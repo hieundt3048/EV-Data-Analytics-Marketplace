bdb6064e513ea64546689d8e95a73395a084c88a
linh <linhtty0051@ut.edu.vn>
2025-10-30 21:18:34 +0700

    AsyncConfigg

---
+++ src/main/java/com/evmarketplace/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+// config/AsyncConfig.java
+package com.evmarketplace.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
